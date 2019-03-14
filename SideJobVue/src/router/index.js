d89dc9ec12d546791a2b1fd6f54771131903a393
EdenKang <kg0116@ojworld.co.kr>
2019-03-14 13:25:43 +0900

    vue-cli : 기본 그리드

--- SideJobVue/src/router/index.js
+++ SideJobVue/src/router/index.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import listView from '@/components/listView'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
+Vue.use(Vuetify)
 Vue.use(Router)
 
 export default new Router({
